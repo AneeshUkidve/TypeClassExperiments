77e9c45ac8f5408e9852e3377e9d3423188f2843
AneeshUkidve <aneeshukidve@gmail.com>
2024-04-29 23:36:50 +0530

    Update IsometryGroup.lean
    Added theorem that isometries are necessarily injective
    
    Realised they are NOT necessarily surjective, and must modify the  ddefinition to reflect the same.

--- DefGroups/IsometryGroup.lean
+++ DefGroups/IsometryGroup.lean
@@ -25,25 +25,48 @@ theorem d_nonneg {α : Type*} [MyMetricSpace α] : ∀ x y : α, 0 ≤ (d x y) :
   ring_nf
   exact h2
 
+@[simp]
+theorem eq_imp_d_zero {α : Type*} [MyMetricSpace α] (x y : α) (h : x = y) : d x y = 0 := by
+  simp [pos_def]
+  exact h
+
 end MyMetricSpace
+
 
 @[ext]
 structure MyIsometry {α : Type*} (inst : MyMetricSpace α) where
   func : α → α
   d_preserve : ∀ x y : α, inst.d x y = inst.d (func x) (func y)
+--Add line for surjective
+
 
 namespace MyIsometry
 
+
 @[simp]
-theorem comp_eq_isometry {α : Type*} (M : MyMetricSpace α) (f g : α → α) (hf : ∀ x y : α, M.d x y = M.d (f x) (f y)) (hg : ∀ x y : α, M.d x y = M.d (g x) (g y)) :
+theorem comp_eq_isometry {α : Type*} [M : MyMetricSpace α] (f g : α → α) (hdf : ∀ x y : α, M.d x y = M.d (f x) (f y)) (hdg : ∀ x y : α, M.d x y = M.d (g x) (g y)) :
   ∀ x y : α, M.d x y = M.d ((f ∘ g) x) ((f ∘ g) y) := by
   intro x y
   simp [Function.comp]
   calc M.d x y
-    _ = M.d (g x) (g y) := by apply hg
-    _ = M.d (f (g x)) (f (g y)) := by apply hf
+    _ = M.d (g x) (g y) := by apply hdg
+    _ = M.d (f (g x)) (f (g y)) := by apply hdf
+
+@[simp]
+theorem func_inj {α : Type*} [M : MyMetricSpace α] (f : MyIsometry M) (x y : α) (h : f.func x = f.func y) : x = y := by
+  apply M.eq_imp_d_zero at h
+  rw [←f.d_preserve] at h
+  rw [M.pos_def] at h
+  exact h
+
+--define inverse function
+
+--define identity function, show it is isometry
 
 def comp {α : Type*} (M : MyMetricSpace α) : MyIsometry M → MyIsometry M → MyIsometry M
-  | MyIsometry.mk f hf, MyIsometry.mk g hg => MyIsometry.mk (f ∘ g) (comp_eq_isometry M f g hf hg)
+  | mk f hdf, mk g hdg => mk (f ∘ g) (comp_eq_isometry f g hdf hdg)
+--Modify proof when surjective condition is added
 
 end MyIsometry
+
+--Show isometries form a group
